28c2ee76a3125048d0eb4c5dd878a02e5951acb1
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-27 15:05:47 +0100

    Footer component created

---
+++ client /src/components /Footer /index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Google Book Store 
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
