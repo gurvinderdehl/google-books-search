54c50a888c77bf3aafcb44a76cd70310dd7008bb
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-27 15:32:51 +0100

    routes edited so they work

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 
 app.use(routes);
 
@@ -22,7 +22,6 @@ mongoose.connect(
     useNewUrlParser: true
   }
 );
-
 
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
